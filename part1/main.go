61f05281b0b8b149e7b630574b8fb6554e10aece
Mike Chastain <mchastain@fuelmedical.com>
2016-06-20 14:32:57 -0700

    working through lesson 1

--- part1/main.go
+++ part1/main.go
@@ -11,11 +11,24 @@ package main
 
 import (
 	"bufio"
-	"log"
 	"net"
 	"net/http"
 )
 
 func main() {
-	// Code goes here.
+	// 1. listen for connections forever
+	if ln, err := net.Listen("tcp", ":8080"); err == nil {
+		//f2. Accept Request
+		if conn, err := ln.Accept(); err == nil {
+			reader := bufio.NewReader(conn)
+
+			if req, err := http.ReadRequest(reader); err == nil {
+				if be, err := net.Dial("tcp", "127.0.0.1:8081"); err == nil {
+					be_reader := bufio.NewReader(be)
+				}
+			}
+
+		}
+
+	}
 }
